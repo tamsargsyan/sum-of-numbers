7a0ed13b12dec9ab239e170f39292a0af1b87b9b
tamsargsyan <62563325+tamsargsyan@users.noreply.github.com>
2020-07-16 10:32:33 +0400

    add sum of numbers

---
+++ main.js
@@ -0,0 +1,9 @@
+// The first way to solve this problem
+let x = Number(prompt("Please enter the first number"));
+let y = Number(prompt("Please enter the second number"));
+alert(x + y);
+
+// The second way to solve this problem
+let a = prompt("1st number") * 1;
+let b = prompt("2nd number") * 1;
+alert(a + b);
